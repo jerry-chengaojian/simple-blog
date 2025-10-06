8ec4fb1a0291684e1b8229e191a352d674168b75
jerry-chengaojian <superchengaojian@gmail.com>
2025-10-06 15:04:20 +0800

    chore: add package.json for module type and update tsconfig for consistent casing

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,11 @@
 import { defineBackend } from "@aws-amplify/backend";
-import { auth } from "./auth/resource.js";
-import { data } from "./data/resource.js";
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
 });
